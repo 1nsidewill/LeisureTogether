b2addde3a3989eb6a8f636fb890020386bb2fded
Yerang Lim <51330260+limyer@users.noreply.github.com>
2024-07-29 22:40:32 +0900

    라이브러리 추가 (webrtc, webview) / yarn만 사용하도록 package-lock.json 삭제

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is an [Expo](https://expo.dev) project created with [`create-expo-app`](htt
 1. Install dependencies
 
    ```bash
-   npm install
+   yarn install
    ```
 
 2. Start the app
@@ -16,14 +16,42 @@ This is an [Expo](https://expo.dev) project created with [`create-expo-app`](htt
     npx expo start
    ```
 
-In the output, you'll find options to open the app in a
+   In the output, you'll find options to open the app in a
 
-- [development build](https://docs.expo.dev/develop/development-builds/introduction/)
-- [Android emulator](https://docs.expo.dev/workflow/android-studio-emulator/)
-- [iOS simulator](https://docs.expo.dev/workflow/ios-simulator/)
-- [Expo Go](https://expo.dev/go), a limited sandbox for trying out app development with Expo
+   - [development build](https://docs.expo.dev/develop/development-builds/introduction/)
+   - [Android emulator](https://docs.expo.dev/workflow/android-studio-emulator/)
+   - [iOS simulator](https://docs.expo.dev/workflow/ios-simulator/)
+   - [Expo Go](https://expo.dev/go), a limited sandbox for trying out app development with Expo
 
-You can start developing by editing the files inside the **app** directory. This project uses [file-based routing](https://docs.expo.dev/router/introduction).
+   You can start developing by editing the files inside the **app** directory. This project uses [file-based routing](https://docs.expo.dev/router/introduction).
+
+3. To Add dependency
+
+   ```bash
+    yarn add ...
+   ```
+
+## Expo Go 사용법
+
+- Expo Go 구글 플레이에서 설치
+- PC와 같은 네트워크일 경우
+
+  ```bash
+   npx expo start
+  ```
+
+  로 실행 후 s를 눌러 expo go 모드로 변경
+
+- PC와 다른 네트워크일 경우
+
+  ```bash
+   npx expo start --tunnel
+  ```
+
+  로 실행 후 s를 눌러 expo go 모드로 변경
+
+- Windows 환경에서는 Windows Defender가 tunnel을 막을 수도 있으므로 예외 등록
+- Scan으로는 잘 동작 안하므로 Expo Go 앱에서 URL을 입력하면 잘 동작할 수 있음
 
 ## Get a fresh project
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,48 +1,50 @@
-import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, View, Button, StyleSheet } from "react-native";
+import { HelloWave } from "@/components/HelloWave";
 
 export default function Index() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Voice Chat Room</Text>
-            </View>
-            <View style={styles.userList}>
-                <Text>User 1</Text>
-                <Text>User 2</Text>
-                <Text>User 3</Text>
-                {/* List more users dynamically here */}
-            </View>
-            <View style={styles.controls}>
-                <Button title="Mute" onPress={() => console.log('Mute/Unmute')} />
-                <Button title="Leave" onPress={() => console.log('Leave Chat')} />
-                {/* Additional control buttons can be added here */}
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Voice Chat Room</Text>
+      </View>
+      <View style={styles.userList}>
+        <Text>User 1</Text>
+        <Text>User 2</Text>
+        <Text>User 3</Text>
+        {/* List more users dynamically here */}
+      </View>
+      <HelloWave />
+      <View style={styles.controls}>
+        <Button title="Mute" onPress={() => console.log("Mute/Unmute")} />
+        <Button title="Leave" onPress={() => console.log("Leave Chat")} />
+        {/* Additional control buttons can be added here */}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-    },
-    header: {
-        padding: 10,
-        backgroundColor: '#6200ee',
-    },
-    headerText: {
-        color: 'white',
-        fontSize: 20,
-    },
-    userList: {
-        flex: 1,
-        alignItems: 'flex-start',
-        marginTop: 20,
-        marginBottom: 20,
-    },
-    controls: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-    }
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  header: {
+    padding: 10,
+    backgroundColor: "#6200ee",
+  },
+  headerText: {
+    color: "white",
+    fontSize: 20,
+  },
+  userList: {
+    flex: 1,
+    alignItems: "flex-start",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  controls: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
 });
